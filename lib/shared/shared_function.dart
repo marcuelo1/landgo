714397616b1f7ffd2a0145f7f6ced3c212dae181
Paul Brian Marcuelo <marcuelo1@gmail.com>
2021-07-16 12:28:00 +0800

    added shared style and functions

---
+++ lib/shared/shared_function.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class SharedFunction {
+  static Color hexToColor(String code){
+    return Color(int.parse(code.substring(0, 6), radix: 16) + 0xFF000000);
+  }
+}
---
+++ lib/shared/shared_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'shared_function.dart';
+
+class SharedStyle {
+  // Check ryve prototype colors first row (from left to right)
+  static final Color lightyellow = SharedFunction.hexToColor("F7F052");
+  static final Color yellow = SharedFunction.hexToColor("F2D450");
+  static final Color darkyellow = SharedFunction.hexToColor("B79332");
+  static final Color brown = SharedFunction.hexToColor("F7F052");
+  static final Color darkbrown = SharedFunction.hexToColor("F7F052");
+
+  // Shades of black, check ryve prototype colors (from left to right) 
+  // Note: some of the colors are the same
+  static final Color dirtyWhite = SharedFunction.hexToColor("3A3A3C");
+  static final Color black = SharedFunction.hexToColor("000000");
+  static final Color black2 = SharedFunction.hexToColor("1C1C1E");
+  static final Color black3 = SharedFunction.hexToColor("2C2C2E");
+  static final Color black4 = SharedFunction.hexToColor("3A3A3C");
+  
+}
