74346d1114db550612f463e48b83465ee33e4132
Paul Brian Marcuelo <marcuelo1@gmail.com>
2021-08-16 23:05:15 +0800

    setup admin authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ###############################
+  #######  A D M I N ############
+  devise_for :admins
+  
+  scope :admin do
+    get "/", to: "admin#index"
+  end
 end
