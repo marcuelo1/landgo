f5ae76f7cc3a7ea3e7dd28a294162822fed11858
Paul Brian Marcuelo <marcuelo1@gmail.com>
2021-08-31 23:51:47 +0800

    setup on mac

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(
+    email: "admin@gmail.com",
+    password: "admin123",
+    first_name: "super",
+    last_name: "admin"
+)
