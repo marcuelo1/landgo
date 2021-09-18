a314e72d9483de2c334f9e3cbaf65e4c3795929e
Paul Brian Marcuelo <marcuelo1@gmail.com>
2021-09-19 06:29:45 +0800

    Added heroku on routes

--- config/routes.rb
+++ config/routes.rb
@@ -85,5 +85,6 @@ Rails.application.routes.draw do
   end
 
   default_url_options :host => "http://192.168.1.9:3000"
+  default_url_options :host => "https://landgo.herokuapp.com"
   # default_url_options :host => "http://localhost:3000"
 end
