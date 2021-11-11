d95dbbbfa1866c5a983b4b0911ba167e6e2fb5eb
paul <marcuelo1@gmail.com>
2021-11-11 20:25:55 +0800

    added brannch to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,8 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 set :keep_releases, 5
 
+set :branch,      fetch(:branch, 'lb-staging')
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
