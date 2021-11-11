8e8ba472b0776187a18cc4b7dfc2b0555cc75250
paul <marcuelo1@gmail.com>
2021-11-11 21:11:12 +0800

    added server url

--- lib/shared/shared_url.dart
+++ lib/shared/shared_url.dart
@@ -1,6 +1,5 @@
 class SharedUrl {
-  static final root = "http://localhost:3000";
-  // static final root = "http://192.168.1.9:3000";
-  // static final root = "https://landgo.herokuapp.com";
+  // static final root = "http://localhost:3000";
+  static final root = "http://159.223.56.152/";
   static final version = "v1";
 }
